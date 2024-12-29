8d9beae4b74b46cc87bcf7eda0ff71104deb7685
Aashu Sharma <buildforfb@gmail.com>
2024-12-29 16:25:27 +0530

    small route changeg

--- app/submission/submissionRoutes.js
+++ app/submission/submissionRoutes.js
@@ -6,7 +6,7 @@ const rootRouter = express.Router();
 const router = express.Router();
 
 router.post("/", createSubmission);
-router.get("/:formId", authenticateUserMiddleware, getFormSubmissions);
+router.get("/form/:formId", authenticateUserMiddleware, getFormSubmissions);
 
 rootRouter.use("/submissions", router);
 
